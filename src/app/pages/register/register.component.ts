f7702e6326cb82da0ca5bcab928d7baf911fe96a
Devisraelmonteiro <149194949+Devisraelmonteiro@users.noreply.github.com>
2025-02-08 08:56:29 -0300

    fix: validacao de formulario e spinner

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -5,7 +5,8 @@ import { UsuarioService } from '../../components/services/usuario.service';
 import { RegisterRequestDTO } from '../../components/models/registre.response.dto';
 import { CommonModule } from '@angular/common';
 import { PasswordValidator } from '../../validators/password.validators';
-
+import { NgxSpinnerService } from 'ngx-spinner';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'app-register',
@@ -23,12 +24,13 @@ export class RegisterComponent {
 
   //atributos
   form: FormGroup; //formulário
-
+  msg: string = ''; //mensagem
 
   //instanciando a classe de serviço de usuário
   constructor(
     private usuarioService: UsuarioService, //injetando o serviço de usuário
-    private fb: FormBuilder //injetando o form builder
+    private fb: FormBuilder, //injetando o form builder
+    private spinner: NgxSpinnerService //injetando o spinner
   ) {
     this.form = this.fb.group({ //criando o formulário
       nome: new FormControl('', [
@@ -64,15 +66,30 @@ export class RegisterComponent {
     };
 
 
+    //exibindo o spinner
+    this.spinner.show();
+
+
     //chamando o serviço de registro de usuário
     this.usuarioService.register(request)
+      .pipe(delay(3000))
       .subscribe({ //capturando a resposta da requisição
         next: (response) => { //caso a requisição seja bem sucedida
+
+
           console.log(response); //exibindo a resposta no console
+          this.msg = 'Usuário cadastrado com sucesso!';
+          this.form.reset(); //resetando o formulário
+          scrollTo(0, 0); //rolando a página para o topo
         },
         error: (e) => { //caso a requisição falhe
+
           console.error(e.error); //exibindo o erro no console
+          this.msg = 'Falha ao cadastrar o usuário';
         }
+      })
+      .add(() => {
+        this.spinner.hide(); //escondendo o spinner
       });
   }
 
